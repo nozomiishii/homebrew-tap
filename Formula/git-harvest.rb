158957b6a716ea8fa80e60d0cc8dc324460dafe2
Nozomi Ishii <nozomiishii.jp@gmail.com>
2026-03-28 14:22:50 +0900

    git-harvest v0.1.4

---
+++ Formula/git-harvest.rb
@@ -0,0 +1,15 @@
+class GitHarvest < Formula
+  desc "Clean up merged branches and worktrees (supports squash merges)"
+  homepage "https://github.com/nozomiishii/git-harvest"
+  url "https://github.com/nozomiishii/git-harvest/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "ebbac474c06f569b94ea54adb2914410d94274d00222fa887ead772a605e1c0c"
+  license "MIT"
+
+  def install
+    bin.install "lib/git-harvest"
+  end
+
+  test do
+    assert_match "git-harvest v#{version}", shell_output("#{bin}/git-harvest --version")
+  end
+end
